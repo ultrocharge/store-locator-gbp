eb771998daf5e979c7858a44b9d7eb9d9d0ec23e
ultrocharge <oliver.elias011@gmail.com>
2024-10-25 16:50:33 +0100

    add new online store file

---
+++ web/assets/online-store.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const container = document.createElement('div');
+    container.innerHTML = `
+      <div>
+        <h1>Welcome to our Online Store!</h1>
+        <p>Browse our collection of products.</p>
+      </div>
+    `;
+    document.body.appendChild(container);
+  });
+  
